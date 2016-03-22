f12b4c3407008799b426f0294d6a8e37efadc6cc
nogaasha <sivosha2010@gmail.com>
2016-03-22 22:46:41 +0300

    redirect to main page

--- app.rb
+++ app.rb
@@ -43,6 +43,6 @@ post '/new' do
   	 return erb :new
   	end
   @db.execute 'insert into Posts (content, created_date) values (?, datetime())',[content]	 
-  erb "Вы ввели #{content}"
+  redirect to '/'
 end
 
