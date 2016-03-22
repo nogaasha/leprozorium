4c6b41bc016335d542f9d0daf901589018e62eac
nogaasha <sivosha2010@gmail.com>
2016-03-22 22:34:30 +0300

    add index.erb

--- app.rb
+++ app.rb
@@ -25,7 +25,11 @@ configure do
 end	
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+#	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+#Выбираем список постов в БД	
+@results = @db.execute 'select * from Posts order by id desc'
+
+erb :index
 end
 
 get '/new' do
